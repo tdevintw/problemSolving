b38e2a1f2881787ae1feebf3a305557930359edc
Yasser <aitelghariyasser@gmail.com>
2024-09-16 19:22:04 +0100

    adding the gcd calculator logic using recursion with java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import Recursion.*;
 
 public class Main {
     public static void main(String[] args) {
-        ArraySum arraySum = new ArraySum();
-        int[] arr = {1,2,3,4,5,6,9};
-        System.out.println(arraySum.arraySum(arr));
+        System.out.println(greatestCommonDivisor.gcd(10205,65));
     }
 }
---
+++ src/Recursion/greatestCommonDivisor.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class greatestCommonDivisor {
+
+    public static  int gcd(int x , int y){
+        if(x%y==0){
+            return y;
+        }else{
+            return gcd(y , x%y);
+        }
+    }
+}
